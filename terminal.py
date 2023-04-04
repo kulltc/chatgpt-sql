ef1fd8bec13d388dbe56e9ae6bd83330c85e7f5c
kulltc <svenwitteveen@gmail.com>
2023-04-04 21:44:30 +0200

    Refactoring main.py file to terminal & controller

---
+++ terminal.py
@@ -0,0 +1,24 @@
+import logging
+from controller import Controller
+
+# Configure the logging settings
+logging.basicConfig(filename='debug.log', filemode='a', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    print("Ask any question about the data. Enter 'q' to quit. Enter 'r' to reset ChatGPT.")
+    controller = Controller()
+    while True:
+        user_input = input("Question: ")
+        if user_input.lower() == 'q':
+            break
+        if user_input == "r":
+            controller.chatModel.reset()
+            continue
+        try:
+            result = controller.run(message=user_input, sender="USER")
+            print(f"ChatGPT: {result}")
+        except ValueError:
+            print("Invalid input. Please enter a number or 'q' to quit.")
+
+if __name__ == "__main__":
+    main()
